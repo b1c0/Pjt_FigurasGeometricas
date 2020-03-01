3a08758ff869df706c8d4622d5dbb8ea250ab8de
b1c0 <bico962009@hotmail.com>
2020-02-29 23:06:09 -0500

    se creo el contenido de la clase Recta

--- Figuras/Recta.py
+++ Figuras/Recta.py
@@ -1,1 +1,22 @@
+import math
+class Recta:
+    def __init__(self,x1,y1,x2,y2):
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
 
+    def longitud(self):
+        a = self.x2-self.x1
+        b = self.y2-self.y1
+        long =  math.sqrt(math.pow(a,2)+math.pow(b,2)) 
+        print("mi longitud es: "+str(long))
+
+    def mover(self,x,y):
+        dx = self.x1-x
+        dy = self.y1-y
+        self.x2 = self.x2-dx
+        self.y2 = self.y2-dy
+        self.x1 = x
+        self.y1 = y
+        print( "La nueva posicion es: ("+str(self.x1)+", "+str(self.y1)+") ("+str(self.x2)+", "+str(self.y2)+")")
---
+++ main.py
@@ -0,0 +1,12 @@
+from Figuras.Recta import *
+
+recta = Recta(1,1,3,1)
+
+print(recta.x1)
+print(recta.y1)
+print(recta.x2)
+print(recta.y2)
+
+recta.longitud()
+
+recta.mover(2,1)
